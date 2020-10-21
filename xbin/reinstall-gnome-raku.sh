5a198b0fa1c4d7fcde1221546b9bc7908774badb
Marcel Timmerman <mt1957@gmail.com>
2020-10-21 14:15:26 +0200

    new program to benchmark installing

--- xbin/reinstall-gnome-raku.sh
+++ xbin/reinstall-gnome-raku.sh
@@ -5,22 +5,32 @@
 #
 export NO_AT_BRIDGE=1
 
-# added to test while using another speaking language to see if there are tests
-# made on english messages which should not be done like that.
-#
-export LC_ALL=fr_FR.UTF-8
-
 zef uninstall Gnome::Gtk3::Glade
 zef uninstall Gnome::Gtk3
 zef uninstall Gnome::Gdk3
 zef uninstall Gnome::Gio
 zef uninstall Gnome::Cairo
-zef uninstall Gnome::Pango
+#zef uninstall Gnome::Pango
 zef uninstall Gnome::GObject
 zef uninstall Gnome::Glib
 zef uninstall Gnome::N
 
-echo -n >> xt/data/Gnome-Install-Times.txt
+echo '' >> xt/data/Gnome-Install-Times.txt
 date >> xt/data/Gnome-Install-Times.txt
 raku -v >> xt/data/Gnome-Install-Times.txt
-/usr/bin/time -v zef -a -o xt/data/Gnome-Install-Times.txt --update install Gnome::Gtk3::Gtk3
+
+# added to test while using another speaking language to see if there are tests
+# made on english messages which should not be done like that.
+export LC_ALL=fr_FR.UTF-8
+
+echo '' >> xt/data/Gnome-Install-Times.txt
+echo 'install Gnome::N Gnome::Glib Gnome::GObject' >> xt/data/Gnome-Install-Times.txt
+/usr/bin/time -a -o xt/data/Gnome-Install-Times.txt zef --update install Gnome::N Gnome::Glib Gnome::GObject
+
+echo '' >> xt/data/Gnome-Install-Times.txt
+echo 'install Gnome::Cairo Gnome::Gio Gnome::Gdk3' >> xt/data/Gnome-Install-Times.txt
+/usr/bin/time -a -o xt/data/Gnome-Install-Times.txt zef --update install Gnome::Cairo Gnome::Gio Gnome::Gdk3
+
+echo '' >> xt/data/Gnome-Install-Times.txt
+echo 'install Gnome::Gtk3' >> xt/data/Gnome-Install-Times.txt
+/usr/bin/time -a -o xt/data/Gnome-Install-Times.txt zef --update install Gnome::Gtk3
